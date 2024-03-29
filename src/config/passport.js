61ae7a2d1c3bb8fe9ead0f02d4cd42f2ac912aa8
Anubhav <anubhavrawat62@gmail.com>
2024-03-29 20:26:37 +0530

    Moved callback url to env

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const config = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 passport.serializeUser((user, done) => {
     done(null, user);
@@ -12,7 +14,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GoogleStrategy({
     clientID: config.CLIENT_ID,
     clientSecret: config.CLIENT_SECRET,
-    callbackURL: "https://good-blue-kitten-coat.cyclic.app/auth/auth/google/callback",
+    callbackURL: process.env.CALLBACK_URL,
     passReqToCallback: true
 },
     function (request, accessToken, refreshToken, profile, done) {
